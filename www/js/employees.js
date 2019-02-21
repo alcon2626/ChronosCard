5336a97808fbf1692312eac9cba28215f66c152b
Leonel Gonzalez <alcon2626@gmail.com>
2019-02-20 19:17:12 -0500

    Jqxgrid
    working grid to hold employees and their time later on

--- www/js/employees.js
+++ www/js/employees.js
@@ -1,1 +1,151 @@
-﻿
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+(function () {
+    "use strict";
+
+    var client, // Connection to the Azure Mobile App backend
+        store,  // Sqlite store to use for offline data sync
+        syncContext, // Offline data sync context
+        tableName = 'System_Users',
+        todoItemTable; // Reference to a table endpoint on backend
+    var username;
+    var password;
+    // Set useOfflineSync to true to use tables from local store.
+    // Set useOfflineSync to false to use tables on the server.
+    var useOfflineSync = false;
+
+    // Add an event listener to call our initialization routine when the host is ready
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    /**
+     * Event Handler, called when the host is ready
+     *
+     * @event
+     */
+    function onDeviceReady() {
+        // Create a connection reference to our Azure Mobile Apps backend 
+        client = new WindowsAzure.MobileServiceClient('https://chronoscard.azurewebsites.net');
+
+        if (useOfflineSync) {
+            initializeStore().then(setup);
+        } else {
+            setup();
+        }
+    }
+
+    /**
+     * Set up and initialize the local store.
+     */
+    function initializeStore() {
+        // Create the sqlite store
+        store = new WindowsAzure.MobileServiceSqliteStore();
+
+        // Define the table schema
+        return store.defineTable({
+            name: tableName,
+            columnDefinitions: {
+                sUSR_ID: 'string',
+                sUSR_Name: 'string',
+                sUSR_Password: 'string',
+                sUSR_Email: 'string',
+                sUSR_logInName: 'string',
+                deleted: 'boolean'
+            }
+        }).then(function () {
+            // Initialize the sync context
+            syncContext = client.getSyncContext();
+
+            // Define an overly simplified push handler that discards
+            // local changes whenever there is an error or conflict.
+            // Note that a real world push handler will have to take action according
+            // to the nature of conflict.
+            syncContext.pushHandler = {
+                onConflict: function (pushError) {
+                    return pushError.cancelAndDiscard();
+                },
+                onError: function (pushError) {
+                    return pushError.cancelAndDiscard();
+                }
+            };
+
+            return syncContext.initialize(store);
+        });
+    }
+
+    /**
+     * Set up the tables, event handlers and load data from the server 
+     */
+    function setup() {
+        var maxwidth = $("#body").width();
+        $("#jqxgrid").css({ "width": maxwidth});
+        // Create a table reference
+        var data = new Array();
+        var firstNames =
+            [
+                "Andrew", "Nancy", "Shelley", "Regina", "Yoshi", "Antoni", "Mayumi", "Ian", "Peter", "Lars", "Petra", "Martin", "Sven", "Elio", "Beate", "Cheryl", "Michael", "Guylene"
+            ];
+        var lastNames =
+            [
+                "Fuller", "Davolio", "Burke", "Murphy", "Nagase", "Saavedra", "Ohno", "Devling", "Wilson", "Peterson", "Winkler", "Bein", "Petersen", "Rossi", "Vileid", "Saylor", "Bjorn", "Nodier"
+            ];
+        var productNames =
+            [
+                "Black Tea", "Green Tea", "Caffe Espresso", "Doubleshot Espresso", "Caffe Latte", "White Chocolate Mocha", "Cramel Latte", "Caffe Americano", "Cappuccino", "Espresso Truffle", "Espresso con Panna", "Peppermint Mocha Twist"
+            ];
+        var priceValues =
+            [
+                "2.25", "1.5", "3.0", "3.3", "4.5", "3.6", "3.8", "2.5", "5.0", "1.75", "3.25", "4.0"
+            ];
+        for (var i = 0; i < 100; i++) {
+            var row = {};
+            var productindex = Math.floor(Math.random() * productNames.length);
+            var price = parseFloat(priceValues[productindex]);
+            var quantity = 1 + Math.round(Math.random() * 10);
+            row["firstname"] = firstNames[Math.floor(Math.random() * firstNames.length)];
+            row["lastname"] = lastNames[Math.floor(Math.random() * lastNames.length)];
+            row["productname"] = productNames[productindex];
+            row["price"] = price;
+            data[i] = row;
+        }
+        var source =
+        {
+            localdata: data,
+            datatype: "array"
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source, {
+            loadComplete: function (data) { },
+            loadError: function (xhr, status, error) { }
+        });
+        $("#jqxgrid").jqxGrid(
+        {
+                source: dataAdapter,
+                width: '100%',
+                autoheight: true,
+                sortable: true,
+                pageable: true,
+                columnsresize: true,
+            columns: [
+                { text: 'First Name', datafield: 'firstname', width: '20%' },
+                { text: 'Last Name', datafield: 'lastname', width: '20%' },
+                { text: 'Product', datafield: 'productname', width: '40%' },
+                { text: 'price', datafield: 'price', width: '20%' }
+            ]
+        });
+    }
+})();
